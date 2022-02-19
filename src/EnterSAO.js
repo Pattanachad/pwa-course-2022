dc83ee99def147acfde07e9b8b6017b81e4b926e
Pattanachad <pattanachad.bunn@bumail.net>
2022-02-19 11:03:23 +0700

    Finish Ex06/01

---
+++ src/EnterSAO.js
@@ -0,0 +1,14 @@
+import "./EnterSAO.css";
+
+function EnterSAO(){
+    return(
+        <div> 
+            <div className="ball">Get</div>
+            <div className="ball">Over</div>
+            <div className="ball">Here</div>
+
+        </div>
+    )
+}
+
+export default EnterSAO;
--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,18 @@
 import { BrowserRouter , Routes , Route } from "react-router-dom";
 import App from "./App";
+import EnterSAO from "./EnterSAO";
+import Profile from "./Profile";
 
 function Router() {
     return(
         <BrowserRouter>
         <Routes>
+            
             <Route path="/" element={ <App/> } />
             <Route path="/profile" element={<Profile/>} />
+            <Route path="/EnterSAO" element={<EnterSAO/>} />
+            
+           
         </Routes>
         </BrowserRouter>
     )
